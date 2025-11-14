de8642ec0e4f18e0c1d489dd4e2de1b11b40db65
elouveau <erwanlouveau.pro@gmail.com>
2025-11-14 10:19:53 +0100

    add mvnpdfsmart_par

---
+++ R/mvnpdfsmart_par.R
@@ -0,0 +1,32 @@
+#' mvnpdfsmart_par
+#'
+#' @param x numeric matrix.
+#' @param mean vector containing each mean of x's columns.
+#' @param varcovM variance covariance matrix of x.
+#' @param Log logical; if TRUE return the logarithm of multivariate distribution.
+#'
+#' @returns a list containing values of x matrix and values of multivariate distribution calculated
+#'
+#' @rdname mvnpdfsmart
+#' @export
+mvnpdfsmart_par <- function(x, mean =  rep(0, nrow(x)),
+                        varcovM = diag(nrow(x)), Log = TRUE) {
+  n <- ncol(x)
+  p <- nrow(x)
+  x0 <- x - mean
+  Rinv <- solve(varcovM)
+  LogDetvarcovM <- log(det(varcovM))
+
+  y <- future.apply::future_sapply(X = 1:n, FUN = function(j) {
+    yj <- - p/2 * log(2*pi) - 0.5 * LogDetvarcovM -
+      0.5 * t(x0[, j]) %*% Rinv %*% x0[, j]
+  })
+
+  if (!Log) {
+    y <- exp(y)
+  }
+
+  res <- list(x = x, y = y)
+  class(res) <- "mvnpdf"
+  return(res)
+}
--- R/mypkgr-package.R
+++ R/mypkgr-package.R
@@ -2,6 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom future.apply future_sapply
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib mypkgr, .registration = TRUE
 ## usethis namespace: end
